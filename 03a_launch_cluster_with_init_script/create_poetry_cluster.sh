af00e2e5133a9007b87c0d25d7b26bbd495be5a0
Yang Wang <yangwang166@gmail.com>
2022-08-30 13:14:47 +1000

    Add auto password gen

--- 03a_launch_cluster_with_init_script/create_poetry_cluster.sh
+++ 03a_launch_cluster_with_init_script/create_poetry_cluster.sh
@@ -1,1 +1,4 @@
+export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domain yanganzssa \
+  --domain-owner 087659795860 --query authorizationToken --output text`
+databricks --profile cse2 secrets put --scope poetry_integration --key password --string-value $CODEARTIFACT_AUTH_TOKEN
 databricks --profile cse2 clusters create --json-file poetry_cluster.json
