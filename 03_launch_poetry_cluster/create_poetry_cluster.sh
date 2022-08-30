a12caee7604989ae2dbf7ebae19978c6e6dd2447
Yang Wang <yangwang166@gmail.com>
2022-08-30 13:15:58 +1000

    update

--- 03_launch_poetry_cluster/create_poetry_cluster.sh
+++ 03_launch_poetry_cluster/create_poetry_cluster.sh
@@ -1,3 +1,4 @@
+#gimme-aws-creds
 export ECRPASSWORD=`aws ecr get-login-password --region ap-southeast-2 --profile=aws-field-eng_databricks-power-user`
 sed "s/xxxxx/${ECRPASSWORD}/g" poetry_cluster.json.tmp > poetry_cluster.json
 databricks --profile cse2 clusters create --json-file poetry_cluster.json
